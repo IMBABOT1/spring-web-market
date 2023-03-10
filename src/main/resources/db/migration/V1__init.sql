ac14cc3dab79c14ae64c0124c90e1ffb90e79dee
unknown <zlovred12@gmail.com>
2023-03-10 17:46:34 +0300

    init

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,24 @@
+create table if not exists products (id bigserial primary key, title varchar(255), price integer);
+
+insert into products (title, title)
+values
+    ('Demon Souls', 100),
+    ('Dark Souls', 100),
+    ('Dark Souls2', 100),
+    ('Dark Souls3', 100),
+    ('BloodBorne', 100),
+    ('Sekiro', 100),
+    ('Elden Ring', 100),
+    ('WoW', 100),
+    ('WoW: Burning Crusade', 100),
+    ('WoW: WOTLK', 100),
+    ('WoW: Cataclysm', 100),
+    ('WoW: MoP', 100),
+    ('WoW: Draenor', 100),
+    ('WoW: Legion', 100),
+    ('WoW: BFA', 100),
+    ('WoW: ShadowLands', 100),
+    ('WoW: DragonFlight', 100),
+    ('Diablo', 100),
+    ('Diablo2', 100),
+    ('Diablo3', 100);
