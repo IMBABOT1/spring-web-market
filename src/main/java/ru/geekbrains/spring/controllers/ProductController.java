b0b5b4c7062105c0914638755f59edcb2c3f92b1
unknown <zlovred12@gmail.com>
2023-03-10 18:26:14 +0300

    temp

--- src/main/java/ru/geekbrains/spring/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/controllers/ProductController.java
@@ -1,17 +1,21 @@
 package ru.geekbrains.spring.controllers;
 
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.spring.entities.Product;
+import ru.geekbrains.spring.exceptions.AppError;
+import ru.geekbrains.spring.exceptions.ResourceNotFoundException;
 import ru.geekbrains.spring.services.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -24,23 +28,23 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> findAll(){
+    public List<Product> findAll() {
         return productService.findAll();
     }
 
 
     @GetMapping("/products/{id}")
-    public Product findProductById(@PathVariable Long id){
-        return productService.findProductById(id);
+    public Product findProductById(@PathVariable Long id) {
+        return productService.findProductById(id).orElseThrow(() -> new ResourceNotFoundException("Product with id not found id: " + id));
     }
 
     @GetMapping("/products/delete/{id}")
-    public void deleteProductById(@PathVariable Long id){
-         productService.deleteProductById(id);
+    public void deleteProductById(@PathVariable Long id) {
+        productService.deleteProductById(id);
     }
 
-//    @GetMapping("/products/change_price")
-//    public void changePrice(@RequestParam Long productId, @RequestParam Integer delta){
-//        productService.changePrice(productId, delta);
-//    }
+    @GetMapping("/products/change_price")
+    public void changePrice(@RequestParam Long productId, @RequestParam Integer delta){
+        productService.changePrice(productId, delta);
+    }
 }
--- src/main/java/ru/geekbrains/spring/services/ProductService.java
+++ src/main/java/ru/geekbrains/spring/services/ProductService.java
@@ -2,10 +2,13 @@ package ru.geekbrains.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.geekbrains.spring.entities.Product;
+import ru.geekbrains.spring.exceptions.ResourceNotFoundException;
 import ru.geekbrains.spring.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -21,12 +24,19 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public Product findProductById(Long id){
-        return productRepository.findById(id).get();
+    public Optional<Product> findProductById(Long id){
+        return productRepository.findById(id);
     }
 
     public void deleteProductById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void changePrice(Long productId, Integer delta) {
+        Product p = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Unable to change product price, id: " + productId));
+        p.setPrice(p.getPrice() + delta);
+
     }
 
 //    public void changePrice(Long productId, Integer delta) {
