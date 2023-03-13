dbbc8f17febdc75d3e4660518de481aa7c6cd2a7
unknown <zlovred12@gmail.com>
2023-03-13 18:52:03 +0300

    dto

--- src/main/java/ru/geekbrains/spring/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/controllers/ProductController.java
@@ -3,29 +3,41 @@ package ru.geekbrains.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.spring.converters.ProductConverter;
+import ru.geekbrains.spring.dto.ProductDto;
 import ru.geekbrains.spring.entities.Product;
 import ru.geekbrains.spring.exceptions.AppError;
 import ru.geekbrains.spring.exceptions.ResourceNotFoundException;
 import ru.geekbrains.spring.services.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
     private ProductService productService;
+    public ProductConverter productConverter;
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
 
+    @Autowired
+    public void setProductConverter(ProductConverter productConverter) {
+        this.productConverter = productConverter;
+    }
 
     @GetMapping
-    public Page<Product> getAllProducts(
+    public Page<ProductDto> getAllProducts(
             @RequestParam(name = "p", defaultValue = "1") Integer page,
             @RequestParam(name = "min_price", required = false) Integer minPrice,
             @RequestParam(name = "max_price", required = false) Integer maxPrice,
@@ -34,7 +46,9 @@ public class ProductController {
         if (page < 1) {
             page = 1;
         }
-        return productService.find(minPrice, maxPrice, titlePart, page);
+
+
+        return productService.find(minPrice, maxPrice, titlePart, page).map(product -> productConverter.entityToDto(product));
     }
 
     @GetMapping("/{id}")
@@ -43,14 +57,14 @@ public class ProductController {
     }
 
     @PostMapping
-    public void addProduct(@RequestBody Product product) {
-        product.setId(null);
-        productService.save(product);
+    public void addProduct(@RequestBody ProductDto productDto) {
+        productDto.setId(null);
+        productService.save(productConverter.dtoToEntity(productDto));
     }
 
     @PutMapping
-    public void editProduct(@RequestBody Product product) {
-        productService.save(product);
+    public void editProduct(@RequestBody ProductDto productDto) {
+        productService.save(productConverter.dtoToEntity(productDto));
     }
 
     @DeleteMapping("/{id}")
